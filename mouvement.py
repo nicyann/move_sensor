e00e90a280637dc4a15a617fba1009569be4b99e
nicyann <yann.nicolle@orange.fr>
2019-02-01 15:18:21 +0000

    fonctionnement du bip

--- mouvement.py
+++ mouvement.py
@@ -7,7 +7,8 @@ broche = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(broche, GPIO.IN)
-
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 currentstate = 0
 previousstate = 0
 
@@ -17,7 +18,12 @@ while True:
     currentstate = GPIO.input(broche)
 		 # Si le capteur est déclenché
     if currentstate == 1 and previousstate == 0:
-        print("    Mouvement détecté !")
+        print("Mouvement détecté")
+        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(22, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(18, GPIO.LOW)
+        GPIO.output(22, GPIO.LOW)
         # En enregistrer l'état
         previousstate = 1
     # Si le capteur est s'est stabilisé
