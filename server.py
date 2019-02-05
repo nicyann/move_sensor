7c64be0a3129d674279dbb813fa696b15f21ff70
nicyann <yann.nicolle@orange.fr>
2019-02-05 10:07:28 +0000

    fichier server.py

--- server.py
+++ server.py
@@ -11,8 +11,6 @@ GPIO.setwarnings(False)
 GPIO.setup(broche, GPIO.IN)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
-currentstate = 0
-previousstate = 0
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -23,26 +21,24 @@ def index():
 
 
 def message_loop():
+    currentstate = 0
+    previousstate = 0
     while True:
-    # Lecture du capteur
-    currentstate = GPIO.input(broche)
-		 # Si le capteur est déclenché
-    if currentstate == 1 and previousstate == 0:
-        message = 'Mouvement détécté, ne bougez plus, les mains en l\'air'
-        socketio.emit('alert', message, Broadcast=True)
-        GPIO.output(18, GPIO.HIGH)
-        GPIO.output(22, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(18, GPIO.LOW)
-        GPIO.output(22, GPIO.LOW)
-        # En enregistrer l'état
-        previousstate = 1
-    # Si le capteur est s'est stabilisé
-    elif currentstate == 0 and previousstate == 1:
-        print("    Prêt")
-        previousstate = 0
-    # On attends 10m
-    time.sleep(0.01)
+        currentstate = GPIO.input(broche)
+        if currentstate == 1 and previousstate == 0:
+            message = 'Mouvement détécté, ne bougez plus, les mains en l\'air'
+            socketio.emit('alert', message, Broadcast=True)
+            GPIO.output(18, GPIO.HIGH)
+            GPIO.output(22, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(18, GPIO.LOW)
+            GPIO.output(22, GPIO.LOW)
+            previousstate = 1
+        elif currentstate == 0 and previousstate == 1:
+            message = 'Ready to move'
+            socketio.emit('alert', message, Broadcast=True)
+            previousstate = 0
+        time.sleep(0.01)
 
 # Vue que notre méthode pour lire nos message est une boucle infinie
 # Elle bloquerait notre serveur. Qui ne pourrait répondre à aucune requête.
